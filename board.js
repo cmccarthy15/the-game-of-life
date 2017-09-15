792218f656a14e660f15455c278be5a1d8d657e9
Caryn McCarthy <cmccarthy15@gmail.com>
2017-09-15 11:14:02 -0400

    updated board.js

--- board.js
+++ board.js
@@ -5,50 +5,62 @@
  * @param {Int} height
  * @param {Array<Int>} cells the array to use for the cells (default: new Uint8Array(width * height))
  */
-function Board(width=32, height=32, cells) {
-  this.width = width
-  this.height = height
-  this.cells = cells || new Uint8Array(width * height)
+function Board(width = 32, height = 32, cells) {
+  this.width = width;
+  this.height = height;
+  this.cells = cells || new Uint8Array(width * height);
 }
 
 Board.prototype.indexFor = function([row, col]) {
-  if (row < 0 || row >= this.height || col < 0 || col >= this.width)
-    return
-  return row * this.width + col
-}
+  if (row < 0 || row >= this.height || col < 0 || col >= this.width) {return;}
+  return row * this.width + col;
+};
 
 Board.prototype.coordsFor = function(index){
   var col = index % this.width;
   var row = (index - col) / this.width;
   return [row, col];
-}
+};
 
 Board.prototype.get = function (coords) {
-  return this.cells[this.indexFor(coords)] || 0
-}
+  return this.cells[this.indexFor(coords)] || 0;
+};
 
 Board.prototype.set = function(coords, value) {
   this.cells[this.indexFor(coords)] = value;
-}
+};
 
 Board.prototype.livingNeighbors = function([row, col]) {
-  let alive = 0;
-  for (let i = row -1; i <= row + 1; i++){ //rows
-    for (var j = col - 1; j <= col + 1; j++){
-      if (this.get([i,j])) alive++;
-    }
-  }
-  if(this.get([row,col])){
-    alive--;
-  }
-  return alive;
-}
+  // let alive = 0;
+  // for (let i = row - 1; i <= row + 1; i++){ //rows
+  //   for (var j = col - 1; j <= col + 1; j++){
+  //     if (this.get([i, j])) alive++;
+  //   }
+  // }
+  // if (this.get([row, col])){
+  //   alive--;
+  // }
+
+  return this.get([row - 1, col - 1]) +
+         this.get([row - 1, col]) +
+         this.get([row - 1, col + 1]) +
+         this.get([row, col - 1]) +
+         this.get([row, col + 1]) +
+         this.get([row + 1, col - 1]) +
+         this.get([row + 1, col]) +
+        this.get([row + 1, col + 1]);
+
+
+
+  //return alive;
+};
 
 Board.prototype.toggle = function(coords) {
   this.set(coords, !this.get(coords));
-}
+  return this; //return this so that you can chain method calls
+};
 
-/** 
+/**
  * @param {Boolean} isAlive
  * @param {Number} numLivingNeighbors
  */
@@ -66,7 +78,7 @@ function conway(isAlive, numLivingNeighbors) {
  * @param {Board!} future (is mutated)
  * @param {(Boolean, Int) -> Boolean} rules (default: conway)
  */
-function tick(present, future, rules=conway) {
+function tick(present, future, rules = conway) {
   var length = present.height * present.width;
   for (var i = 0; i < length; i++){
     var coords = present.coordsFor(i);
@@ -74,5 +86,4 @@ function tick(present, future, rules=conway) {
   }
   return [future, present];
 }
-
 
